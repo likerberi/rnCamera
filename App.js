c4b5a65f48433bef569172f0bb21615ff7d682a4
stepbystep <likerberi97@gmail.com>
2019-08-08 18:14:22 +0900

    rev from RN docu

--- App.js
+++ App.js
@@ -4,6 +4,18 @@ import { RNCamera } from 'react-native-camera';
 
 export default class App extends PureComponent {
   
+  takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+  
+  barcodeRecognized = ({barcodes}) => {
+    barcodes.forEach(barcode => console.warn(barcode.data))
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,41 +23,14 @@ export default class App extends PureComponent {
           ref={ref => {
             this.camera = ref;
           }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
+          style={{
+            flex: 1,
+            width: '100%'
           }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
+          onGoogleVisionBarcodesDetected={this.barcodeRecognized}
         />
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-            <Text style={{ fontSize: 15 }}> SNAP </Text>
-          </TouchableOpacity>
-        </View>
       </View>
     );
-  }
-
-  takePicture = async() => {
-    if (this.camera) {
-      const options = { quality: 0.7, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
-  };
 }
 
 const styles = StyleSheet.create({
